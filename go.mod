641ff6a5e6b53e75f3e821be3e294ebce5bd8c05
Kyle Wilson <wilson15701@gmail.com>
2025-11-07 09:09:25 -0500

    rm tests for now

--- go.mod
+++ go.mod
@@ -4,8 +4,6 @@ go 1.24.5
 
 require (
 	github.com/gorilla/websocket v1.5.4-0.20250319132907-e064f32e3674
-	github.com/onsi/ginkgo/v2 v2.22.0
-	github.com/onsi/gomega v1.36.1
 	go.uber.org/mock v0.6.0
 	k8s.io/api v0.34.0
 	k8s.io/apimachinery v0.34.0
@@ -33,13 +31,11 @@ require (
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
-	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/btree v1.1.3 // indirect
 	github.com/google/cel-go v0.26.0 // indirect
 	github.com/google/gnostic-models v0.7.0 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
-	github.com/google/pprof v0.0.0-20241029153458-d1b30febd7db // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -80,7 +76,6 @@ require (
 	golang.org/x/term v0.34.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
 	golang.org/x/time v0.9.0 // indirect
-	golang.org/x/tools v0.36.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
--- internal/controller/healthcheck_controller_test.go
+++ internal/controller/healthcheck_controller_test.go
@@ -16,69 +16,69 @@ limitations under the License.
 
 package controller
 
-import (
-	"context"
+// import (
+// 	"context"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+// 	. "github.com/onsi/ginkgo/v2"
+// 	. "github.com/onsi/gomega"
+// 	"k8s.io/apimachinery/pkg/api/errors"
+// 	"k8s.io/apimachinery/pkg/types"
+// 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+// 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	healthv1alpha1 "github.com/kdwils/constellation/api/v1alpha1"
-)
+// 	healthv1alpha1 "github.com/kdwils/constellation/api/v1alpha1"
+// )
 
-var _ = Describe("HealthCheck Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+// var _ = Describe("HealthCheck Controller", func() {
+// 	Context("When reconciling a resource", func() {
+// 		const resourceName = "test-resource"
 
-		ctx := context.Background()
+// 		ctx := context.Background()
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		healthcheck := &healthv1alpha1.HealthCheck{}
+// 		typeNamespacedName := types.NamespacedName{
+// 			Name:      resourceName,
+// 			Namespace: "default", // TODO(user):Modify as needed
+// 		}
+// 		healthcheck := &healthv1alpha1.HealthCheck{}
 
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind HealthCheck")
-			err := k8sClient.Get(ctx, typeNamespacedName, healthcheck)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &healthv1alpha1.HealthCheck{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
+// 		BeforeEach(func() {
+// 			By("creating the custom resource for the Kind HealthCheck")
+// 			err := k8sClient.Get(ctx, typeNamespacedName, healthcheck)
+// 			if err != nil && errors.IsNotFound(err) {
+// 				resource := &healthv1alpha1.HealthCheck{
+// 					ObjectMeta: metav1.ObjectMeta{
+// 						Name:      resourceName,
+// 						Namespace: "default",
+// 					},
+// 					// TODO(user): Specify other spec details if needed.
+// 				}
+// 				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+// 			}
+// 		})
 
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &healthv1alpha1.HealthCheck{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
+// 		AfterEach(func() {
+// 			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+// 			resource := &healthv1alpha1.HealthCheck{}
+// 			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+// 			Expect(err).NotTo(HaveOccurred())
 
-			By("Cleanup the specific resource instance HealthCheck")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &HealthCheckReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
+// 			By("Cleanup the specific resource instance HealthCheck")
+// 			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+// 		})
+// 		It("should successfully reconcile the resource", func() {
+// 			By("Reconciling the created resource")
+// 			controllerReconciler := &HealthCheckReconciler{
+// 				Client: k8sClient,
+// 				Scheme: k8sClient.Scheme(),
+// 			}
 
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
-		})
-	})
-})
+// 			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+// 				NamespacedName: typeNamespacedName,
+// 			})
+// 			Expect(err).NotTo(HaveOccurred())
+// 			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+// 			// Example: If you expect a certain status condition after reconciliation, verify it here.
+// 		})
+// 	})
+// })
